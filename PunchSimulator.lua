78cac31ab6397df3cf8a1252718fe2433dda2b5d
MasterGamerCat1 <142456678+MasterGamerCat1@users.noreply.github.com>
2023-08-22 18:28:41 -0700

    PunchSimulator.lua

---
+++ PunchSimulator.lua
@@ -0,0 +1,29 @@
+getgenv().RHF_Settings = {
+    StartupNotification = false, -- Toast Notification On Startup: default = true
+    StartupAnimation = false, -- Loading Animation On Startup: default = true
+    ErrorToastNotifications = false-- Toast Notifications For Errors: default = false
+}
+ 
+RubyHubFunctions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RubyScriptsOnTop/RubyHubFunctions/main/source.lua"))()
+ 
+task.wait(2)
+ 
+RubyHubFunctions.StartupAnimation({
+    Title = "Chester Hub", -- default = "Ruby Hub Functions"
+    LoadingText = "Thank you for choosing Chester Hub!", -- default = "Loading..."
+    Icon = "rbxassetid://14515446164" -- default = RubyHubFunctions.Icons.RubyHubNoBG
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MasterGamerCat1/MasterGamerCat-Scripts/main/PunchSim.lua"))()
+ 
+task.wait(3)
+ 
+while task.wait(0.5) do
+ 
+    pcall(function()
+ 
+
+ 
+    end)
+ 
+end
