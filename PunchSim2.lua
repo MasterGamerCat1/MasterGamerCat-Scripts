7da1a4d9b467e0d979dec3874b3700a39a7a1de4
MasterGamerCat1 <142456678+MasterGamerCat1@users.noreply.github.com>
2023-08-25 19:09:18 -0700

    Update PunchSim2.lua

--- PunchSim2.lua
+++ PunchSim2.lua
@@ -22,8 +22,7 @@ while task.wait(0.5) do
  
     pcall(function()
  
-game.Players.LocalPlayer:Kick("This Script Is No Longer Supported Join (gg/Gc63gEUsRQ)")
-
+game.Players.LocalPlayer:Kick("This Script Is No Longer Supported Join For New Version Of Script. Thanks For Using MasterGamerCat HUB (gg/Gc63gEUsRQ)")
  
     end)
  
