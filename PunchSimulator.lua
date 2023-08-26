7bf756cb84ab86020477b16aa5af2bf2f8f2eee0
MasterGamerCat1 <142456678+MasterGamerCat1@users.noreply.github.com>
2023-08-25 19:03:59 -0700

    Update PunchSimulator.lua

--- PunchSimulator.lua
+++ PunchSimulator.lua
@@ -9,18 +9,20 @@ RubyHubFunctions = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ru
 task.wait(2)
  
 RubyHubFunctions.StartupAnimation({
-    Title = "Chester Hub", -- default = "Ruby Hub Functions"
-    LoadingText = "Thank you for choosing Chester Hub!", -- default = "Loading..."
-    Icon = "rbxassetid://14515446164" -- default = RubyHubFunctions.Icons.RubyHubNoBG
+    Title = "MasterGamerCat Hub", -- default = "Ruby Hub Functions"
+    LoadingText = "Thank you for choosing MasterGamerCat Hub!", -- default = "Loading..."
+    Icon = "rbxassetid://5281294369" -- default = RubyHubFunctions.Icons.RubyHubNoBG
 })
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MasterGamerCat1/MasterGamerCat-Scripts/main/PunchSim.lua"))()
+
+task.wait(4)
  
-task.wait(3)
- 
-while task.wait(5) do
+while task.wait(0.5) do
  
     pcall(function()
  
-game.Players.LocalPlayer:Kick("This Script Is No Longer Supported Join (gg/Gc63gEUsRQ)")
+
  
     end)
  
